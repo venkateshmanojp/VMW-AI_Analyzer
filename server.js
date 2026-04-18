461a20ec6e2d3f8c56ad53195bc8e459add620db
venkateshmanojp <venkateshmanojp@gmail.com>
2026-04-18 13:04:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const express  = require("express");
 const fetch    = require("node-fetch");
 const app      = express();
 
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({limit: "50mb", extended: true}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
